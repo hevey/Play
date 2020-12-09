6a9c908525aa7098807d3ffcd1d94799246c838b
Glenn Hevey <glenn.hevey@gmail.com>
2020-12-09 20:15:55 +1100

    Initial project structure

---
+++ Play/PlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayApp.swift
+//  Play
+//
+//  Created by Glenn Hevey on 9/12/20.
+//
+
+import SwiftUI
+
+@main
+struct PlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
